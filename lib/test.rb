68ad5f70f6543d8f86a5ed42a1df8f8557531e85
empathetic-search-0228 <muneeb.dahab@outlook.com>
2020-11-08 03:14:57 +0000

    Automatically backed up by Learn

--- lib/test.rb
+++ lib/test.rb
@@ -1,7 +1,7 @@
 def am_the_goat
 puts "Am the f**kin goat here"
-puts phrase 
-puts phrase 
+puts phrase
+puts phrase
 puts phrase
 puts phrase
 puts phrase
@@ -11,4 +11,3 @@ puts phrase
 puts phrase
 puts phrase
 end
-
